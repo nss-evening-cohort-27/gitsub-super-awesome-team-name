d6bf43b0dc00641acecad0b1c3e16ddad2d6185e
jmmyfam <pham_j@ymail.com>
2024-05-11 10:09:41 -0500

    made data for repos

---
+++ scripts/repositories.js
@@ -0,0 +1,38 @@
+let repos = [
+  {
+    id: 1,
+    title: 'E27-Sorting-Hat',
+    desc: 'Individual project to emulate Pet Adoption',
+    lang: 'JavaScript'
+  },
+  {
+    id: 2,
+    title: 'E27-Pet-Adoption',
+    desc: 'Exercise to CRUD',
+    lang: 'JavaScript'
+  },
+  {
+    id: 3,
+    title: 'E27-Link-In-Bio',
+    desc: 'Practice using Bootstrap',
+    lang: 'HTML'
+  },
+  {
+    id: 4,
+    title: 'E27-Product-Cards',
+    desc: 'Introduction to Bootstrap cards',
+    lang: 'HTML'
+  },
+  {
+    id: 5,
+    title: 'E27-Simple-Calculator',
+    desc: 'Flexbox exercise',
+    lang: 'HTML'
+  },
+  {
+    id: 6,
+    title: 'E27-HTML-Resume',
+    desc: 'Builing HTML version of your resume',
+    lang: 'HTML'
+  },
+];
