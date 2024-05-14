875bf9162ea931770d3f2052c4aa433a315f316f
asparadise33 <abby.paradise@gmail.com>
2024-05-13 20:53:31 -0500

    projects page completed

--- scripts/projects.js
+++ scripts/projects.js
@@ -2,52 +2,74 @@ const projects =[
   {
   id: 1,
   title: "Example 1 Private",  
-  info: "No desciprtion",
+  info: "No description",
 },
 {
   id: 2,
   title: "my-goals",  
-  info: "No desciprtion",
+  info: "No description",
 },
 {
   id: 3,
   title: "Sample my Goals",  
-  info: "No desciprtion",
+  info: "goals for the NSS Bootcamp",
 } 
 ]
 
 const renderToDom = (divId, htmlToRender) => {
-const selectedDiv = document.querySelector(divId)
-selectedDiv.innerHTML = htmlToRender}
+  const selectedDiv = document.querySelector(divId);
+  selectedDiv.innerHTML = htmlToRender;
+};
 
 const projectsOnDom =(projects) => {
-let projectString =""
+let projectString ="";
 projects.forEach((project) => {
   projectString += `
   <div class="card">
   <div class="card-body">
-    <p>${project.title}</p>
-    <p>${project.info}</p>
+    <p class ="card-title">${project.title}</p>
+    <p class ="card-info">${project.info}</p>
   </div>
 </div>`
   })
   renderToDom("#projects", projectString)
 }
+projectsOnDom(projects)
 
 const formOnDom = () => {
-  const formProject = `<div class="mb-3">
-  <h3>Create a new project</h3>
-  <h4>Coordinate, track, and update your work in one place, so projects stay transparaent and on schedule.</h4>
+  let formInfo= `<form id ="projectForm">
+  <h5>Create a new project</h5>
+  <h6>Coordinate, track, and update your work in one place, so projects stay transparaent and on schedule.</h6>
+  <div id ="projectForm" class="mb-3">
   <label for="formGroupExampleInput" class="form-label">Project Board Name</label>
-  <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Example 2">
-</div>`
-renderToDom("#projectForm", formProject)
+  <input type="text" class="form-control" id="title" placeholder="Example 2">
+  <label for="formGroupExampleInput" class="form-label">Description(optional)</label>
+  <input type="text" class="form-control" id="info" placeholder="">
+  <button type="submit" id ="create" class="btn btn-success">Create project</button>
+</div>
+</form>`;
+renderToDom("#projectForm", formInfo)
 }
+formOnDom()
 
+
+  const createProject = (e) => {
+    e.preventDefault();
+    const newProject = {
+      id: projects.length +1, 
+      title: document.querySelector("#title").value, 
+      info: document.querySelector("#info").value,
+    }
+    projects.push(newProject) 
+    projectsOnDom(projects)
+   document.querySelector("#projectForm").reset()
+    
+  }
+  form.addEventListener("submit", createProject)
 //event listener
-const projectButton = document.querySelector("#proj")
+// const createButton = document.querySelector("#submit")
 
-projectButton.addEventListener("click", () => {
-  projectsOnDom ()
-  formOnDom ()
-})
+// createButton.addEventListener("click", () => {
+//   projectsOnDom(projects)
+//   formOnDom ()
+// })
